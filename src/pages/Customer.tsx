eb489b812fcd90cf73fbe7dcff9623f9317e7058
VNKaiSer <votandat.dev@gmail.com>
2025-03-26 10:18:22 +0700

    feat: delete agency

--- src/pages/Customer.tsx
+++ src/pages/Customer.tsx
@@ -1,3 +1,4 @@
+import { DeleteAgency } from "@/apis/agency";
 import {
   Table,
   TableBody,
@@ -5,17 +6,18 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table'
-import { AgencyContext } from '@/contexts/AgencyContext'
-import { AgencyContextType } from '@/utils/types'
-import { useContext } from 'react'
-import { FaCubes, FaEdit, FaPlus } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+} from "@/components/ui/table";
+import { toast } from "@/components/ui/use-toast";
+import { AgencyContext } from "@/contexts/AgencyContext";
+import { AgencyContextType } from "@/utils/types";
+import { useContext } from "react";
+import { FaEdit, FaPlus, FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Customer = () => {
   const { updateAgency, agencies } = useContext(
     AgencyContext
-  ) as AgencyContextType
+  ) as AgencyContextType;
 
   return (
     <>
@@ -45,7 +47,28 @@ const Customer = () => {
         </TableHeader>
         <TableBody>
           {agencies.map((customer, index) => {
-            const color = index % 2 === 0 ? 'bg-gray-100' : 'bg-white'
+            const color = index % 2 === 0 ? "bg-gray-100" : "bg-white";
+            const handleDeleteAgency = () => {
+              const confirm = window.confirm("Chắc chắn xoá đại lý này?");
+              if (confirm) {
+                DeleteAgency(customer.id + "")
+                  .then((_) => {
+                    toast({
+                      title: "Success",
+                      variant: "success",
+                      description: "Xoá đại lý thành công",
+                    });
+                    window.location.reload();
+                  })
+                  .catch((error) => {
+                    toast({
+                      title: "Error",
+                      variant: "destructive",
+                      description: error.message,
+                    });
+                  });
+              }
+            };
             return (
               <TableRow
                 key={index}
@@ -62,20 +85,21 @@ const Customer = () => {
                 </TableCell>
                 <TableCell
                   onClick={() => updateAgency(customer)}
-                  className="text-right border"
+                  className="text-right border flex justify-center gap-2"
                 >
-                  <Link to={'form?agency_id=' + customer.id}>
+                  <FaTrash onClick={handleDeleteAgency} />
+                  <Link to={"form?agency_id=" + customer.id}>
                     <FaEdit />
                   </Link>
                 </TableCell>
               </TableRow>
-            )
+            );
           })}
         </TableBody>
       </Table>
       <div className="w-full flex justify-end mt-2">
         <Link
-          to={'form'}
+          to={"form"}
           className="bg-[#1ABB9C] border border-[#1ABB9C] text-white p-0 h-fit px-2 rounded-md"
         >
           <FaPlus className="inline" />
@@ -83,7 +107,7 @@ const Customer = () => {
         </Link>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Customer
+export default Customer;
